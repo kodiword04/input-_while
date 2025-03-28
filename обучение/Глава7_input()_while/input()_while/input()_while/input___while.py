760ae1745afb99856ffb21c7ac3e60232e50c0a6
alex <alex@HOME-PC>
2025-03-28 11:54:01 +0300

    задача 7.5

--- обучение/Глава7_input()_while/input()_while/input()_while/input___while.py
+++ обучение/Глава7_input()_while/input()_while/input()_while/input___while.py
@@ -1,35 +1,51 @@
-﻿#7.1 input()
-print('#7.1\n')
-#car = input('What car do you want to take: ')
-#print(f'Let me see if I can find you a {car.title()}')
+﻿# #7.1 input()
+# print('#7.1\n')
+# car = input('What car do you want to take: ')
+# print(f'Let me see if I can find you a {car.title()}')
 
-#7.2 input() � int()
-print('\n#7.2')
+# #7.2 input() � int()
+# print('\n#7.2')
 
-#num_of_seats = input('How many seats do you need?\n')
+# num_of_seats = input('How many seats do you need?\n')
 
-#if int(num_of_seats) >= 8:
-    #print('Sorry but you need to wait a few hours')
-#else:
-   # print('You are welcome!')
+# if int(num_of_seats) >= 8:
+#     print('Sorry but you need to wait a few hours')
+# else:
+#     print('You are welcome!')
 
-#7.3 input() � int()
-#print('\n#7.3')
-#number = input('Tell me a number and I tell you is it multiply 10 or not\n')
-#if int(number) % 10 == 0:
-    #print(f'\t{number} is multiply 10: ')
-#else:
-    #print(f'\t{number} is not multiply 10')
+# #7.3 input() � int()
+# print('\n#7.3')
+# number = input('Tell me a number and I tell you is it multiply 10 or not\n')
+# if int(number) % 10 == 0:
+#     print(f'\t{number} is multiply 10: ')
+# else:
+#     print(f'\t{number} is not multiply 10')
 
-#7.4 while
-print('\n#7.4')
-message = 'Hello! Please enter topping for your pizza.\nIf you are done, enter "quit": '
+# #7.4 while
+# print('\n#7.4')
+# message = '\nHello! Please enter topping for your pizza.\nIf you are done, enter "quit": '
+
+# active = True
+# while active:
+#     topping = input(message)
+
+#     if topping == 'quit':
+#         active = False
+#     else:
+#         print(f'Added: {topping}')
+
+#7.5 while()
+message = '\nTell your age and I tell you a ticket cost.\nIf you are done, enter "quit": '
 
 active = True
 while active:
-    topping = input(message)
-
-    if topping == 'quit':
-        break
+    age = input(message)
+    if age == 'quit':
+        active = False
     else:
-        print(f'Added: {topping}')
+        if int(age) < 3:
+            print('Ticket cost 0')
+        elif int(age) < 12:
+            print('Ticket cost is 10 dollars')
+        else:
+            print('Ticket cost is 15 dollars')
