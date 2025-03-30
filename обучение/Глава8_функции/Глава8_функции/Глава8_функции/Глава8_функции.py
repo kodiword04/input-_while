26c1b493ed6b33f04cdab1fc37916e3873a8f238
sasha <sasha@KOMPUTER>
2025-03-30 17:24:43 +0300

    задача 8.2: передача параметра функции

--- обучение/Глава8_функции/Глава8_функции/Глава8_функции/Глава8_функции.py
+++ обучение/Глава8_функции/Глава8_функции/Глава8_функции/Глава8_функции.py
@@ -1,6 +1,17 @@
 #8.1 создание и вывод функции
+print('\n#8.1')
+
 def display_message(message):
     print(message)
 
 message = '8.1 is about functions!'
 display_message(message)
+
+#8.2 передача параметра функции
+print('\n#8.2')
+
+def favorite_book(title):
+    print(f'One of my favorite book is {title.title()}')
+
+title = input('What is you favorite book? ')
+favorite_book(title)
