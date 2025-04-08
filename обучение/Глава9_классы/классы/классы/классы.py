8532bd21c21ff29e1d08c0bdb681687346cc662a
alex <alex@HOME-PC>
2025-04-08 22:59:52 +0300

    задача 9.3: создание класса и использование методов

--- обучение/Глава9_классы/классы/классы/классы.py
+++ обучение/Глава9_классы/классы/классы/классы.py
@@ -1,4 +1,7 @@
 #9.1 создание класса и использование методов
+from pydoc import describe
+
+
 print('#9.1')
 
 class Restaurant:
@@ -26,4 +29,34 @@ restaurant_1 = Restaurant('tokio city','asian')
 restaurant_1.describe_restaurant()
 
 restaurant_2 = Restaurant('vostok','russian')
-restaurant_2.describe_restaurant()
+restaurant_2.describe_restaurant
+
+#9.3 создание класса и использование методов
+print('\n#9.3')
+
+class User:
+    def __init__(self,first_name,last_name,location,nickname):
+        self.first = first_name
+        self.last = last_name
+        self.location = location
+        self.nickname = nickname
+
+    def describe_user(self):
+        print(f"\n{self.first.title()} {self.last.title()}'s nickname is {self.nickname}")
+        print(f"His location is {self.location.title()}")
+
+    def greet_user(self):
+        print(f"Hello, {self.nickname}!")
+
+
+user_0 = User('alex','gafarov','spb','kodiword')
+user_0.describe_user()
+user_0.greet_user()
+
+user_1 = User('zlata','gafarova','spb','zlatik228')
+user_1.describe_user()
+user_1.greet_user()
+
+user_2 = User('kirill','zooravlev','ufa','sprint')
+user_2.describe_user()
+user_2.greet_user()
