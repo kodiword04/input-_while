5dab44b1924962c70d30fde50de74e7b0da4db1d
alex <alex@HOME-PC>
2025-04-09 21:52:53 +0300

    задача 9.4: изменение класса restaurant и добавление новых методов с изменением атрибутов

--- обучение/Глава9_классы/классы/классы/классы.py
+++ обучение/Глава9_классы/классы/классы/классы.py
@@ -1,19 +1,25 @@
 #9.1 создание класса и использование методов
-from pydoc import describe
-
-
 print('#9.1')
 
 class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.name = restaurant_name
         self.type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f'{self.name.title()} has {self.type} cuisine')
 
     def open_restaurant(self):
         print('The restaurant is open!')
+    
+    def set_number_served(self, served):
+        self.number_served = served
+        print(f'Served clients: {self.number_served}')
+    
+    def increment_number_served(self,plus_to_number_served):
+        self.number_served += plus_to_number_served
+        print(f'Served clients are grown now: {self.number_served}')
 
 restaurant = Restaurant('euroasia','asian')
 restaurant.describe_restaurant()
@@ -60,3 +66,16 @@ user_1.greet_user()
 user_2 = User('kirill','zooravlev','ufa','sprint')
 user_2.describe_user()
 user_2.greet_user()
+
+#9.4 изменение класса restaurant и добавление новых методов с изменением атрибутов
+print('\n#9.4')
+
+my_restaurant = Restaurant('euroasia','asian')
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
+my_restaurant.number_served = 100
+print(my_restaurant.number_served)
+
+my_restaurant.set_number_served(500)
+
+my_restaurant.increment_number_served(30)
