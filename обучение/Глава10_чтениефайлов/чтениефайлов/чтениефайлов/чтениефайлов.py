7816905f6c38b526165234301ce608923c13de09
alex <alex@HOME-PC>
2025-04-17 12:32:53 +0300

    задача 10.1: чтение файлов и вывод содержимого из них

---
+++ обучение/Глава10_чтениефайлов/чтениефайлов/чтениефайлов/чтениефайлов.py
@@ -0,0 +1,21 @@
+#10.1 чтение файлов и вывод содержимого из них
+print('#10.1')
+
+file = 'learning_python.txt'
+with open(file) as file_object:
+    string = file_object.read()
+
+print(string.rstrip())
+
+print('')
+
+with open(file) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+print('')
+
+with open(file) as file_object:
+    lines = file_object.readlines()
+    for line in lines:
+        print(line.rstrip())
